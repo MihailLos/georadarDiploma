1fa66eee15adccb18a667d8f3770540d0a29a7ea
Oleg <773o@mail.ru>
2024-04-21 22:38:47 +0700

    Small fixes

--- visualization.py
+++ visualization.py
@@ -11,29 +11,19 @@ class Radargramm:
     def read_from_segy(self, filepath):
         stream = obspy.read(filepath)
         data = np.array([trace.data for trace in stream])
-        return pd.DataFrame(data).T  # Транспонируем для корректной работы с данными
+        return pd.DataFrame(data).T
 
     def interpolate_datasets(self, df1, df2, traces_between):
-        # Проверяем, что количество трасс в обоих датасетах одинаково
         if df1.shape[1] != df2.shape[1]:
             raise ValueError("Datasets must have the same number of traces")
 
-        # Рассчитываем новое количество трасс
-        new_trace_count = df1.shape[1] + df2.shape[1] + traces_between
-
-        # Инициализируем массив для интерполированных данных
-        interpolated_data = np.zeros((df1.shape[0], new_trace_count))
-
-        # Заполняем первый датасет
-        interpolated_data[:, :df1.shape[1]] = df1
-
-        # Начальный индекс для второго датасета
+        total_traces = df1.shape[1] + df2.shape[1] + traces_between
+        interpolated_data = np.zeros((df1.shape[0], total_traces))
+        interpolated_data[:, :df1.shape[1]] = df1.values
         start_second_dataset = df1.shape[1] + traces_between
 
-        # Заполняем данные второго датасета
-        interpolated_data[:, start_second_dataset:start_second_dataset + df2.shape[1]] = df2
+        interpolated_data[:, start_second_dataset:start_second_dataset + df2.shape[1]] = df2.values
 
-        # Интерполируем между последней трассой первого и первой трассой второго датасета
         start_value = df1.iloc[:, -1]
         end_value = df2.iloc[:, 0]
         for i in range(df1.shape[0]):
@@ -42,20 +32,32 @@ class Radargramm:
 
         return pd.DataFrame(interpolated_data)
 
-    def visualize(self, data):
+    def visualize(self, data, start_interp=None, end_interp=None, colormap='gray'):
         plt.figure(figsize=(10, 6))
-        plt.imshow(data, aspect='auto', interpolation='none', cmap='gray')
-        plt.colorbar(label='Amplitude')
+        im = plt.imshow(data, aspect='auto', interpolation='none', cmap=colormap)
+        plt.colorbar(im, label='Amplitude')
         plt.xlabel('Traces')
         plt.ylabel('Samples')
         plt.title('Interpolated Radargram')
+
+        if start_interp is not None and end_interp is not None:
+            plt.axvline(x=start_interp, color='red', linestyle='--', linewidth=2)
+            plt.axvline(x=end_interp, color='red', linestyle='--', linewidth=2)
+
         plt.show()
+
+    def interpolate_and_visualize(self, file1, file2, traces_between):
+        df1 = self.read_from_segy(file1)
+        df2 = self.read_from_segy(file2)
+        interpolated_data = self.interpolate_datasets(df1, df2, traces_between)
+        start_interp = df1.shape[1]
+        end_interp = start_interp + traces_between
+        self.visualize(interpolated_data, start_interp=start_interp, end_interp=end_interp)
 
 
 # Example usage
 radargramm = Radargramm()
-data1 = radargramm.read_from_segy("initial_data/Пресный_водоём.seg")
-data2 = radargramm.read_from_segy("initial_data/Пресный_водоём.seg")  # Возможно, у вас другой файл
 traces_between = int(input("Enter the number of new traces to interpolate between existing traces: "))
-interpolated_data = radargramm.interpolate_datasets(data1, data2, traces_between)
-radargramm.visualize(interpolated_data)
+radargramm.interpolate_and_visualize("initial_data/Пресный_водоём.seg",
+                                     "initial_data/Пресный_водоём.seg",
+                                     traces_between)
